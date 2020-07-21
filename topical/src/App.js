8f6e6c2dc5744eafcd2f996641d0c036dfa416f1
amgillenwater <amgillenwater@gmail.com>
2020-07-21 10:43:21 -0400

    Added material to set foundation for root page

--- topical/src/App.js
+++ topical/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import './App.css'
 import Search from './components/Search'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
 // import Login from './components/Login'
 // import Registration from './components/Registration'
 
 function App () {
   return (
-    <div>
-      <Search />
-    </div>
+    <Container maxWidth='sm'>
+      <Typography variant='h2' gutterBottom>
+        Topical
+      </Typography>
+      <Typography variant='h4' gutterBottom>
+        To get started, scan a product:
+      </Typography>
+      <Typography variant='body1' gutterBottom>
+        Add barcode component
+      </Typography>
+      <Typography variant='h4' gutterBottom>
+        Or search by product name:
+      </Typography>
+      <Typography variant='body1' gutterBottom>
+        Add name search component
+      </Typography>
+    </Container>
   )
 }
 
--- topical/src/components/Search.js
+++ topical/src/components/Search.js
@@ -3,12 +3,12 @@ import React, { useState } from 'react'
 // import Autocomplete from '@material-ui/lab/Autocomplete'
 import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box'
+import axios from 'axios'
 
 const Search = (props) => {
   const [productName, setProductName] = useState('')
   const [upc, setUpc] = useState(0)
 
-  
   return (
     <Box>
       <Button raised>Submit</Button>
